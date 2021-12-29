61d241d78b59921c4567887fb3e50fe480d8008d
DOODOORAM <91834528+DOODOORAM@users.noreply.github.com>
2021-12-29 09:48:39 -0500

    Add files via upload

---
+++ PRO-C22-SA-Boilerplate_code-main/js/Cannon.js
@@ -0,0 +1,21 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonimg=loadImage('assets/canon.png')
+    this.cannonBaseimg=loadImage('assets/cannonBase.png')
+
+
+  }
+  display(){
+    push()
+    imageMode(CENTER)
+image(this.cannonimg,this.x, this.y,this.width,this.height)
+pop()
+image(this.cannonBaseimg,79,20,200,200)
+noFill();
+  }
+}
